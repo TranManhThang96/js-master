334d394f9634c2611508527017db314b92af1a2e
TranManhThang <tranmanhthang96@gmail.com>
2019-09-14 12:35:21 +0700

    babel async await

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("thang beo dep hihi");
+function async_one() {
+    return '1';
+}
+
+function async_two() {
+    return '2';
+}
+
+async function async_three() {
+    let a = await async_two();
+    console.log(a);
+    console.log(async_one());
+}
+
+async_three();
